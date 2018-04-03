5f026704061fc45b64e28f26e7bcc01ebfda43ab
Sudhanshu patel <sudhanshuptl13@gmail.com>
2018-04-03 23:36:24 +0530

    basic template and rendring
    day two basic rendring

--- project_1/music/urls.py
+++ project_1/music/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import  url
 
 urlpatterns = [
     url(r'^$',views.index, name='index'), #path doesnot support regular exp
+
+    #music/1 where 1 is id
+    url(r'^(?P<album_id>[0-9]+)/$', views.details,name='detail')
 ]
--- project_1/music/views.py
+++ project_1/music/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .models import Album,Song
 
 from django.http import HttpResponse
+from django.template import  loader
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Music App homepage</h1>')
+    all_album = Album.objects.all();
+    template = loader.get_template('music/index.html')
+
+    #context used to share data to template
+    context = {
+                'all_album': all_album,
+              }
+    return HttpResponse(template.render(context,request))
+
+
+def details(request,album_id):
+    return HttpResponse("<h2>Details for Album : "+str(album_id)+"</h2>")
+
 
