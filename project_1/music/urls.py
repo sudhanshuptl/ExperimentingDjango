17476fc2218a3597fddfe7260394ca6d3c1660a9
Sudhanshu patel <sudhanshuptl13@gmail.com>
2018-04-05 21:51:19 +0530

    dynamic url

--- project_1/music/urls.py
+++ project_1/music/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^$',views.index, name='index'), #path doesnot support regular exp
 
     #music/1 where 1 is id
-    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='detail')
+    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='detail'),
+
+    # music/id/favorite
+    #url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
